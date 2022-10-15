60e2dc05dc8f08c10c3f25e82c1dd218fbf34123
tomiis4 <87276646+tomiis4@users.noreply.github.com>
2022-10-15 14:14:24 +0200

    Create App.java

---
+++ draw-lines-wrong-way/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+	public static void main(String[] args) {
+    // Create  frame
+    try {
+      CreateFrame CreateFrame = new CreateFrame();
+      System.out.println("\n[FRAME] Frame was created");
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+	}
+}
